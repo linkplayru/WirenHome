d945f07b9266e77cd4417d0034ae73ec8703fbff
linkplayru <linkplay@yandex.ru>
2025-03-16 20:29:07 +0300

    add sprut template for button mode & improve ButtonMode script

--- ButtonMode.js
+++ ButtonMode.js
@@ -1,28 +1,35 @@
 var ButtonMode = function(itemName) {
   /*UI*/
   var cellsObject = {};
-  cellsObject["protect"] = {
-    title: {en: 'protect', ru: 'Только двойное нажатие'},
-    type: "switch",
-    value: false,
-    readonly: false,
-    order: 1
+  cellsObject["normal"] = {
+      title: "Нормальный режим",
+      type: "switch",
+      value: false,
+      readonly: false,
+      order: 1
   }
-  cellsObject["off"] = {
-    title: {en: 'off', ru: 'Отключить выключатели'},
+  cellsObject["protect"] = {
+    title: "Двойной режим",
     type: "switch",
     value: false,
     readonly: false,
     order: 2
   }
+  cellsObject["off"] = {
+    title: "Режим блокировки",
+    type: "switch",
+    value: false,
+    readonly: false,
+    order: 3
+  }
   cellsObject["status"] = {
-    title: {en: 'status', ru: 'Режим работы'},
+    title: "Режим работы",
     type: "range",
 	value: 0,
 	min: 0,
 	max: 2,
 	readonly: true,
-	order: 3
+	order: 4
   }
   defineVirtualDevice(itemName, {
     title: {en: itemName, ru: 'Режим работы выключателей'},
@@ -30,21 +37,34 @@ var ButtonMode = function(itemName) {
   });
 
   defineRule({
-    whenChanged: ["{}/protect".format(itemName), "{}/off".format(itemName)],
+    whenChanged: ["{}/normal".format(itemName), "{}/protect".format(itemName), "{}/off".format(itemName)],
     then: function(value, device, param) {
       if (value) {
-        if (param == "protect") {
+        if (param == "normal") {
+          getDevice(itemName).getControl("protect").setValue({value:false, notify:false});
+          getDevice(itemName).getControl("off").setValue({value:false, notify:false});
+          dev[itemName]["status"] = 0;
+        } else if (param == "protect") {
+          getDevice(itemName).getControl("normal").setValue({value:false, notify:false});
           getDevice(itemName).getControl("off").setValue({value:false, notify:false});
           dev[itemName]["status"] = 1;
         } else if (param == "off") {
+          getDevice(itemName).getControl("normal").setValue({value:false, notify:false});
           getDevice(itemName).getControl("protect").setValue({value:false, notify:false});
           dev[itemName]["status"] = 2;
         }
       } else {
+        getDevice(itemName).getControl("normal").setValue({value:true, notify:false});
         dev[itemName]["status"] = 0;
       }
     }
   });
+
+  //init
+  getDevice(itemName).getControl("normal").setValue({value:true, notify:false});
+  getDevice(itemName).getControl("protect").setValue({value:false, notify:false});
+  getDevice(itemName).getControl("off").setValue({value:false, notify:false});
+  dev[itemName]["status"] = 0;
 }
 
 new ButtonMode("ButtonMode");
